a7404eb249977dcb3a36279b31266ce99daeaf17
Patrick1986Git <patryk.kozlowski@wp.pl>
2026-05-14 06:33:10 +0200

    Use minimal Spring Boot config in observability validation test

--- src/test/java/com/company/shop/observability/ObservabilityConfigValidationTest.java
+++ src/test/java/com/company/shop/observability/ObservabilityConfigValidationTest.java
@@ -4,13 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.company.shop.ShopApplication;
 
 import io.micrometer.core.instrument.MeterRegistry;
 
-@SpringBootTest(classes = ShopApplication.class)
+@SpringBootTest(classes = ObservabilityConfigValidationTest.TestApplication.class)
 class ObservabilityConfigValidationTest {
 
     @Autowired
@@ -35,5 +35,10 @@ class ObservabilityConfigValidationTest {
         meterRegistry.counter("shop.webhook.total", "result", "received").increment();
 
         assertThat(meterRegistry.get("shop.webhook.total").tag("result", "received").counter()).isNotNull();
+    }
+
+    @SpringBootConfiguration
+    @EnableAutoConfiguration
+    static class TestApplication {
     }
 }
