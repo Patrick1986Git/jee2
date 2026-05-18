611e7b1a4f96b28914985cfd9c9b47240b5c7bb4
Patrick1986Git <patryk.kozlowski@wp.pl>
2026-05-18 21:09:24 +0200

    test: fix OpenApiDocsSmokeTest boot context loading

--- src/test/java/com/company/shop/config/OpenApiDocsSmokeTest.java
+++ src/test/java/com/company/shop/config/OpenApiDocsSmokeTest.java
@@ -19,10 +19,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -52,7 +52,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
                         + "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
         }
 )
-@ContextConfiguration(classes = OpenApiDocsSmokeTest.OpenApiDocsSmokeTestConfiguration.class)
+@Import(OpenApiDocsSmokeTest.OpenApiDocsSmokeTestConfiguration.class)
 @AutoConfigureMockMvc
 class OpenApiDocsSmokeTest {
 
@@ -110,7 +110,7 @@ class OpenApiDocsSmokeTest {
                 .anyMatch(path -> path.matches("^/api/v1/admin/categories/\\{[^/]+\\}$"));
     }
 
-    @Configuration
+    @TestConfiguration
     static class OpenApiDocsSmokeTestConfiguration {
 
         @Bean
